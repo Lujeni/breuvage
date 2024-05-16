ecee2e17c30c52fe545ed615f4094d3f86a6c12f
THEBAULT Julien <julien@thebault.co>
2024-05-16 09:48:28 +0200

    feat(exercism): 04

---
+++ 02-exercism/workspaces/elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -0,0 +1,18 @@
+defmodule FreelancerRates do
+  def daily_rate(hourly_rate) do
+    hourly_rate * 8 / 1
+  end
+
+  def apply_discount(before_discount, discount) do
+    before_discount - ((before_discount / 100) * discount)
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    ceil(apply_discount(daily_rate(hourly_rate) * 22, discount))
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    days = apply_discount(daily_rate(hourly_rate), discount)
+    Float.floor(budget / days, 1)
+  end
+end
