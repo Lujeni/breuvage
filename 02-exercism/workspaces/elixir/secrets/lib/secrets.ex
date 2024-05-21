3caf1ee639759cdbf47bfc8e307ce7ec9fb02dbc
THEBAULT Julien <julien@thebault.co>
2024-05-21 18:04:50 +0200

    feat(exercism): 05

---
+++ 02-exercism/workspaces/elixir/secrets/lib/secrets.ex
@@ -0,0 +1,29 @@
+defmodule Secrets do
+  def secret_add(secret) do
+    &(&1 + secret)
+  end
+
+  def secret_subtract(secret) do
+    &(&1 - secret)
+  end
+
+  def secret_multiply(secret) do
+    &(&1 * secret)
+  end
+
+  def secret_divide(secret) do
+    &(trunc(&1 / secret))
+  end
+
+  def secret_and(secret) do
+    &(Bitwise.band(secret, &1))
+  end
+
+  def secret_xor(secret) do
+    &(Bitwise.bxor(&1, secret))
+  end
+
+  def secret_combine(secret_function1, secret_function2) do
+    fn x -> secret_function2.(secret_function1.(x)) end
+  end
+end
