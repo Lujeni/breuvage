bbbf6157f714162afe7f82fe668a1bebe82e8a98
THEBAULT Julien <julien.thebault@numberly.com>
2024-06-04 18:49:50 +0200

    feat(exercism): 17

---
+++ 02-exercism/workspaces/elixir/take-a-number/lib/take_a_number.ex
@@ -0,0 +1,20 @@
+defmodule TakeANumber do
+  def start() do
+    spawn(fn -> loop(0) end)
+  end
+  defp loop(state) do
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, state)
+        loop(state)
+      {:take_a_number, sender_pid} ->
+        new_state = state + 1
+        send(sender_pid, new_state)
+        loop(new_state)
+      :stop ->
+        nil
+      _ ->
+        loop(state)
+    end
+  end
+end
