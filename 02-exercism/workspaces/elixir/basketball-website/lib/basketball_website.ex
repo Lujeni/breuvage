371809a0e641b692efb6225cf029d04cda8ca3cc
THEBAULT Julien <julien.thebault@numberly.com>
2024-06-16 21:41:07 +0200

    feat(exercism): 22

---
+++ 02-exercism/workspaces/elixir/basketball-website/lib/basketball_website.ex
@@ -0,0 +1,14 @@
+defmodule BasketballWebsite do
+  def extract_from_path(data, path) do
+    do_extract_from_path(data, String.split(path, "."))
+  end
+
+  defp do_extract_from_path(data, []), do: data
+  defp do_extract_from_path(data, [x | xs]) do
+    do_extract_from_path(data[x], xs)
+  end
+
+  def get_in_path(data, path) do
+    get_in(data, String.split(path, "."))
+  end
+end
